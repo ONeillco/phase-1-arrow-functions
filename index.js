ad9e4fd449d0d754df0acfdf0be3da16efd593bb
Cory <oneillco117@gmail.com>
2021-06-08 21:29:47 -0400

    commit tree ea740eb6aa82e30c862dedf4e265c1f37ea6b228

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+return 2000 / 100;
+ 
+}
+// let x = 2;
+//  square = x => `${2} * ${8}`;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
+
+//const nums = [8];
+let x = 2;
+const square = (x => x * 2); 
+
+ 
